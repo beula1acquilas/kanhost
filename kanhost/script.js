31dca11fd8d8604a668cb768a38d9cc3313a6eb5
beula1acquilas <73038581+beula1acquilas@users.noreply.github.com>
2020-10-26 15:39:33 +0200

    Add files via upload

---
+++ kanhost/script.js
@@ -0,0 +1,14 @@
+document.getElementById("foot01").innerHTML=
+"<p>&copy; " + new Date().getFullYear() + "DSIA Group. All Rights Reseverd.</p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='Home Page.html'>Home</a></li>" +
+"<li><a href='Data.html'>Data</a></li>" +
+"<li><a href='About.html'>About</a></li>" +
+"<li><a href='what.html'>Login</a></li>" +
+"<li><a href='what2.html'>Sign Up</a></li>" +
+"</ul>";
+
+
+
