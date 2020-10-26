a07c72350ea7d883832e39c7c1faad86e591dfbe
beula1acquilas <73038581+beula1acquilas@users.noreply.github.com>
2020-10-26 15:30:11 +0200

    Add files via upload

---
+++ doc/login.php
@@ -0,0 +1,127 @@
+<html>
+<head>
+      <title>DSIA Group</title>
+      <link rel="stylesheet" type="text/css" href="style.css">
+      <link rel="stylesheet" type="text/css" href="bootstrap.css">
+      <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+      <link rel="stylesheet" type="text/css" href="w3.css">
+      <link rel="stylesheet" type="text/css" href="site.css">
+      
+</head>
+
+<body>
+
+<div id="main">
+
+  <img src="Logo.png" alt="Logo" style="width:5%" ><h2>Welcome to Digital Skills In Action Group (DSIA Group)</h2>
+
+  <p>
+    Our Club teachs Programming Languages and also the basic of ICT. If you are new to our club click here to <a href="what2.html" target="_self">register</a>.<br>
+     <div class="w3-dropdown-hover"> 
+    <button class="w3-btn w3-lime">Visit our Language site Programming Languages</button>
+      <div class="w3-dropdown-content w3-border">
+        <a href="html 5.html">HTML 5</a>
+        <a href="css.html">CSS</a>
+        <a href="java script.html">Javascript</a>
+      </div>
+    </div>
+
+  </p>
+
+  <h2>Vision</h2>
+
+  <p>To enable ICT proficient students, for ICT lies at the core of every organization today. Our vision is also to be more than just observers of technological development. Rather, we must create technological catalysts in our own environment.</p>
+  
+  <h2>Staff Members.</h2>
+
+  <p>
+    1. Nambakumana Issac - The President Of DSIA Group<br>
+    2. SHIMO Anne Divine - The Vise President Of DSIA Group<br>
+    3. KANAMUGIRE Beula Acquilas - The Secretary Of DSIA Group<br>
+    4. Rosslin Angelo - Assistant Secretary<br>
+  </p>
+
+  <h2>Mission</h2>
+
+  <p>The club aims to improve the ICT skills of its members by nurturing their creative and programming skills, and sharpening their research and presentation skills. The club also aims to promote innovation among its members as they seek out technological 
+    solutions to problems that may arise in a school environment</p>
+   
+  <h2>Goals And Objectives</h2>
+
+  <p>
+    • To help other students become competent and confident users of ICT through training sessions. <br>
+    • To develop policies concerning the use of technology at school.<br>
+    • To help students develop an appreciation for the role of ICT in today's society.<br>
+    • To help students develop as innovative beings who will see problems and create solutions.<br>
+    • To provide a fun and interactive approach to ICT.<br>
+    • To raise awareness and promote the use of technology as a teaching/learning aid.<br>
+    • To provide ICT club students with sufficient skills to find solutions for in-house needs, such as timetable creation, duty allocation etc..<br>
+    • To encourage students to develop as innovative beings, transforming them into confident and competent users.<br>
+    • To encourage students should develop their own website and thereby turn into young entrepreneurs.<br>
+    • To encourage students to propose their innovative ideas to the world through their participation in various coding events and bringing laurels to the institution.<br>
+
+  </p>
+
+  <h2>Meeting Day</h2>
+
+  <p>Wednesday 15:00 PM - 16:00 PM</p>
+
+  <div class="container">
+<div class="login-box">
+    <div class="row">
+    <div class="col-md-6 login-left">
+         <h2> Login Here</h2>
+         <form action="validation.php" method="post" netlify>
+           <div class="form-group">
+             <label>Username</label>
+             <input type="text" name="user" class="form-control" required>
+            </div>
+           <div class="form-group">
+           <label>Password</label>
+           <input type="password" name="password" class="form-control" required>
+           </div>
+           <button type="submit" class="btn btn-primary"> Login </button>
+         </form>
+    </div>
+
+    <div class="col-md-6 login-right">
+         <h2> Register Here</h2>
+         <form action="registration.php" method="post" netlify>
+           <div class="form-group">
+             <label>Username</label>
+             <input type="text" name="user" class="form-control" required>
+            </div>
+           <div class="form-group">
+           <label>Password</label>
+           <input type="password" name="password" class="form-control" required>
+           </div>
+           <button type="submit" class="btn btn-primary"> Register </button>
+         </form>
+    </div>
+    </div>
+</div>
+  <footer id="foot01"></footer>
+</div>
+
+<script src="script.js"></script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+
+
+
+
+
+</html>
